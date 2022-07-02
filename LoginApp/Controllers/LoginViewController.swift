641e4835fdbffa0d625cf68207d91dfc313a9a86
Alexkarav <djmyaso@mail.ru>
2022-07-02 16:41:42 +0300

    Refactoring

--- LoginApp/Controllers/LoginViewController.swift
+++ LoginApp/Controllers/LoginViewController.swift
@@ -12,14 +12,15 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
-    private let login = "Alexey"
-    private let password = "GoodTeacher"
+    private let login = "User"
+    private let password = "User123"
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    //MARK: Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
         welcomeVC.login = login
@@ -31,7 +32,7 @@ class LoginViewController: UIViewController {
 
     //MARK: IBActions
     @IBAction func loginButtonPressed(_ sender: Any) {
-        if loginTF.text == "" && passwordTF.text == "" {
+        if loginTF.text == "" || passwordTF.text == "" {
             showAllert(title: "Password or login is empty",
                        message: "Please, insert your login or password")
         } else if loginTF.text != login && passwordTF.text != password {
@@ -52,11 +53,6 @@ class LoginViewController: UIViewController {
         showAllert(title: "Hi, Mate!", message: "Your password is \(password)")
     }
     
-    //MARK: Hide KeyBoard
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super .touchesBegan(touches, with: event)
-    }
-    
     //MARK: Private functions
     private func showAllert(title: String, message: String, textField: UITextField? = nil) {
         let alert = UIAlertController(title: title,
@@ -69,6 +65,14 @@ class LoginViewController: UIViewController {
         
         alert.addAction(okAction)
         present(alert, animated: true)
+    }
+}
+
+// MARK: Hide keyboard aftertouches
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
 }
 
